a413d44c2ba3d82262e789853235f7763932dfbc
Logan <490211581@qq.com>
2020-05-06 16:48:47 +0800

    临时提交

---
+++ app/src/main/java/com/logan/android/ui/layout/constraint/ConstraintMainActivity.kt
@@ -0,0 +1,23 @@
+package com.logan.android.ui.layout.constraint
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.logan.android.ui.R
+
+/**
+ * desc: Constraint layout main <br/>
+ * time: 2020/5/6 11:43 AM <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+class ConstraintMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_relative)
+
+        // TODO Logan
+        // 进入ConstraintRelativeActivity 页面
+    }
+
+}
