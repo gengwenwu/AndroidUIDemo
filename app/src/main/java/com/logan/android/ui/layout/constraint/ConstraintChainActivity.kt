94ddec6b2b69b83e170f5fa5ef086e2644fc96d4
Logan <490211581@qq.com>
2020-05-07 14:01:06 +0800

    完善chain 备注

--- app/src/main/java/com/logan/android/ui/layout/constraint/ConstraintChainActivity.kt
+++ app/src/main/java/com/logan/android/ui/layout/constraint/ConstraintChainActivity.kt
@@ -16,7 +16,6 @@ class ConstraintChainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constraint_circle)
 
-
         // 1，chain
         // 如果两个或以上控件通过下面的方式约束在一起，就可以认为是它是一条链（图为横向的链)
         //   || <-- TextVew1  <--->  TextView2 <--->  TextView3 ||
@@ -36,6 +35,20 @@ class ConstraintChainActivity : AppCompatActivity() {
         //      (1)，app:layout_constraintHorizontal_weight // 横向
         //      (2)，app:layout_constraintVertical_weight // 竖向
         // 注意，如果设置上面两个配置，width必须是0dp才有效果。
+
+        // 3，当我们使用 match_constraint(width或height等于0dp)，ConstraintLayout 将对控件进行 2 次测量，
+        // ConstraintLayout在1.1中可以通过设置 layout_optimizationLevel 进行优化，可设置的值有：
+        //      (1)、none：无优化
+        //      (2)、standard：仅优化直接约束和屏障约束（默认）
+        //      (3)、direct：优化直接约束
+        //      (4)、barrier：优化屏障约束
+        //      (5)、chain：优化链约束
+        //      (6)、dimensions：优化尺寸测量
+
+        // 4，left、right 与 start、end 效果基本一样，但是建议使用后两个代替前两个(支持阿语UI布局)。
+        //    做国内市场可以无所谓。但是有一点需要注意：要么使用left、right或start、end。
+        //    不要混搭使用，譬如：使用marginLeft的同时，又使用marginEnd，可能会有布局问题。
+
     }
 
 }
