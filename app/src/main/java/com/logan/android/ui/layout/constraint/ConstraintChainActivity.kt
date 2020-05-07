61f22f8eb4bc9fe4f5c45d2a70d1119be52f105f
Logan <490211581@qq.com>
2020-05-07 11:50:35 +0800

    完善chain demo 带有margin

--- app/src/main/java/com/logan/android/ui/layout/constraint/ConstraintChainActivity.kt
+++ app/src/main/java/com/logan/android/ui/layout/constraint/ConstraintChainActivity.kt
@@ -16,6 +16,18 @@ class ConstraintChainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constraint_circle)
 
+        // 如果两个或以上控件通过下面的方式约束在一起，就可以认为是他们是一条链（图为横向的链)
+        //   || <-- TextVew1  <--->  TextView2 <--->  TextView3 ||
+
+        // Chain有两个方向横向、竖向。对应约束布局分别是：
+        //      (1)，app:layout_constraintHorizontal_chainStyle
+        //      (2)，app:layout_constraintVertical_chainStyle
+        //
+        // 它们分别对应有3个值，分别是：
+        // (1)、spread：均分占比(两端不靠边)
+        // (2)、spread_inside：均分占比(两端靠边)
+        // (3)、packed：所有元素集中靠在一起，两端留空。
+
     }
 
 }
