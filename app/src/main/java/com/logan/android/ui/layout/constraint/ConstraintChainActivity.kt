c490df8a7b94d2aed5c1f7e71dcd10a59643b927
Logan <490211581@qq.com>
2020-05-07 13:47:53 +0800

    完善chain weight

--- app/src/main/java/com/logan/android/ui/layout/constraint/ConstraintChainActivity.kt
+++ app/src/main/java/com/logan/android/ui/layout/constraint/ConstraintChainActivity.kt
@@ -16,7 +16,9 @@ class ConstraintChainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constraint_circle)
 
-        // 如果两个或以上控件通过下面的方式约束在一起，就可以认为是他们是一条链（图为横向的链)
+
+        // 1，chain
+        // 如果两个或以上控件通过下面的方式约束在一起，就可以认为是它是一条链（图为横向的链)
         //   || <-- TextVew1  <--->  TextView2 <--->  TextView3 ||
 
         // Chain有两个方向横向、竖向。对应约束布局分别是：
@@ -27,7 +29,13 @@ class ConstraintChainActivity : AppCompatActivity() {
         // (1)、spread：均分占比(两端不靠边)
         // (2)、spread_inside：均分占比(两端靠边)
         // (3)、packed：所有元素集中靠在一起，两端留空。
+        // Chain配合margin使用的时候，先margin，剩下的距离再均分。
 
+
+        // 2，在设置chain的同时，也可以设置权重，配置如下：
+        //      (1)，app:layout_constraintHorizontal_weight // 横向
+        //      (2)，app:layout_constraintVertical_weight // 竖向
+        // 注意，如果设置上面两个配置，width必须是0dp才有效果。
     }
 
 }
