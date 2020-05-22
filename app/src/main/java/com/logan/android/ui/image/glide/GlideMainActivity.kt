8b5635d897f367f086d5a96dd181314fa2bb2712
Logan <490211581@qq.com>
2020-05-22 16:19:12 +0800

    Glide加载不同数据格式

--- app/src/main/java/com/logan/android/ui/image/glide/GlideMainActivity.kt
+++ app/src/main/java/com/logan/android/ui/image/glide/GlideMainActivity.kt
@@ -27,6 +27,10 @@ class GlideMainActivity : BaseActivity() {
         val ERROR_URL = "http://tiebapic.baidu.com/09fac7db.jpg"
 
         //
+        val URL_GIF_125_KB_1680_580 =
+            "https://imcut.jollychic.com/uploads/jollyimg/imageService/img/bizz/2020/02/01/12/30/fe62f691-d2cb-4ebf-8fac-a4f423acd7ac.gif"
+
+        //
         val URL_IAMGE_8_KB_144_144 =
             "https://imglf5.nosdn.127.net/img/blJBWWNxcUllaVllbTlLdWF2YWdsQWdrdEFhUFRXZE1hWHphWWdKZm8xd3dwdkN1QkVTQ05nPT0.jpg?imageView&thumbnail=180x180&quality=90&type=jpg"
 
@@ -114,7 +118,6 @@ class GlideMainActivity : BaseActivity() {
                 //      3. 加载失败显示 ic_error。
                 //      4. 如果 url 为 null（一定得是 null ），则显示图片 ic_fallback。
             }),
-
             // 3, 加载指定大小的图片
             ButtonModel("加载指定大小的图片", View.OnClickListener {
                 // 关于图片尺寸
@@ -142,8 +145,34 @@ class GlideMainActivity : BaseActivity() {
                 Glide.with(context).load(IMAGE_BIG).apply(options).into(imageView)
                 // 不建议这样做，因为比如原始图片大小 1024*1024，而 ImageView 是 128*128，
                 // 那么两种加载方式占用的内存可能会相差几十倍。
-            })
+            }),
 
+            // 4，加载不同格式：Gif、Bitmap、Drawable、File
+            ButtonModel("加载不同格式", View.OnClickListener {
+                // 在 Glide4.0 中有一个 RequestBuilders 的泛型类，用于指定加载资源的格式，可以通过下面四种方法指定。
+                // 默认情况下，如果我们不指定，则是得到一个 RequestBuilders 对象。
+                // 1, asDrawable() 得到 RequestBuilders 对象。
+                // 2, asGif() 得到 RequestBuilders 对象。
+                // 3, asBitmap() 得到 RequestBuilders 对象。
+                // 4, asFile() 得到 RequestBuilders 对象。
+
+                val options: RequestOptions = RequestOptions()
+                    .placeholder(R.drawable.ic_default).error(R.drawable.ic_error)
+                    .override(com.bumptech.glide.request.target.Target.SIZE_ORIGINAL)
+
+                // 加载gif
+                Glide.with(context).asGif().load(URL_GIF_125_KB_1680_580).apply(options)
+                    .into(imageView)
+
+                // 加载bitmap
+                // Glide.with(context).asBitmap().load(IMAGE_BIG).apply(options).into(imageView)
+
+                // 加载drawable
+                // Glide.with(context).asDrawable().load(IMAGE_BIG).apply(options).into(imageView)
+
+                // 加载File TODO
+                // Glide.with(context).asFile().load(IMAGE_BIG)
+            })
         )
     }
 }
