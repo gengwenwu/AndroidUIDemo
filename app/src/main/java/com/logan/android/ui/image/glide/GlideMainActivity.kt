f80b420acd745712abc0ec09d0ec484a1333ba31
Logan <490211581@qq.com>
2020-05-22 13:49:35 +0800

    优化Glide 默认图片

--- app/src/main/java/com/logan/android/ui/image/glide/GlideMainActivity.kt
+++ app/src/main/java/com/logan/android/ui/image/glide/GlideMainActivity.kt
@@ -2,7 +2,6 @@ package com.logan.android.ui.image.glide
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -24,17 +23,30 @@ import com.logan.android.ui.tool.dp2px
 class GlideMainActivity : BaseActivity() {
 
     companion object {
+        // 错误image地址
+        val ERROR_URL = "http://tiebapic.baidu.com/09fac7db.jpg"
 
+        //
+        val URL_IMAGE_75_KB =
+            "https://imcut.jollychic.com//uploads/jollyimg/imageService/img/goods/2019/08/21/14/30/b6db0215-5a1d-4c80-974c-9c0d7fb6255c.jpg"
+
+        //
+        val URL_IMAGE_900_KB = "https://farm6.staticflickr.com/5344/9109117816_4de9739e71_k.jpg"
+
+        //
         val URL_IMAGE_2_MB =
-            "http://tiebapic.baidu.com/forum/abpic/item/55cbecec8a13632785695253868fa0ec09fac7db.jpg"
-        val URL_IMAGE_901_KB = "https://farm6.staticflickr.com/5344/9109117816_4de9739e71_k.jpg"
-        val URL_IMAGE_132_KB = "http://tiebapic.baidu.com/forum/w%3D580/sign=49ef71fcdcef76093c0b99971edda301/4e6f47087bf40ad127c2b282402c11dfa9ecce45.jpg"
+            "https://images.unsplash.com/photo-1479689836735-bd21a38cbffd?ixlib=rb-0.3.5&q=99&fm=jpg&crop=entropy&cs=tinysrgb&w=2048&fit=max&s=ac300ebd9dd7b3beb51635a80fd4347c"
 
-        val IMAGE_SMALL = URL_IMAGE_132_KB
-        val IMAGE_MIDDLE = URL_IMAGE_901_KB
+        // 小质量图片
+        val IMAGE_SMALL = URL_IMAGE_75_KB
+
+        // 中等质量图片
+        val IMAGE_MIDDLE = URL_IMAGE_900_KB
+
+        // 大质量图片
         val IMAGE_BIG = URL_IMAGE_2_MB
-
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,27 +69,30 @@ class GlideMainActivity : BaseActivity() {
             ButtonModel("默认占位符", View.OnClickListener {
                 // 2.1 默认占位符
                 // 如果加载的图片时间比较长，那么可以设置一个默认的图片
-                val options: RequestOptions =
-                    RequestOptions().placeholder(R.drawable.ic_default)
-                Glide.with(context).load(IMAGE_SMALL).apply(options).into(imageView)
+                val options: RequestOptions = RequestOptions()
+                    .placeholder(R.drawable.ic_default)
+                Glide.with(context).load(IMAGE_BIG).apply(options).into(imageView)
             }),
             ButtonModel("错误占位符", View.OnClickListener {
                 // 2.2  错误占位符
                 // 如果图片不能成功加载，就显示特定的图片
                 val optionsError: RequestOptions = RequestOptions()
-                    .placeholder(R.drawable.ic_default).error(R.drawable.ic_error)
-                Glide.with(context).load(IMAGE_SMALL).apply(optionsError).into(imageView)
+                    .placeholder(R.drawable.ic_default)
+                    .error(R.drawable.ic_error)
+                Glide.with(context).load(ERROR_URL).apply(optionsError).into(imageView)
             }),
             ButtonModel("错误占位符(errorUrl)", View.OnClickListener {
-                // 从 Glide 4.3.0 开始，可以使用 error API 来指定一个 RequestBuilder，
-                // 在主请求失败的时候开始一次新的加载 (即：加载 errorUrl 地址)。
-                val errorUrl = "失败要加载的地址"
+                // 从 Glide 4.3.0 开始，可以使用 error api 来指定一个 RequestBuilder，
+                // 在主请求失败的时候开始一次新的加载，即：加载 errorUrl 地址。
+                val backupUrlWhenLoadUrlError =
+                    "https://imcut.jollychic.com/uploads/jollyimg/imageMaterialLib/201906/28/IL201906281038536038.jpg"
+
                 Glide.with(context)
-                    .load(IMAGE_SMALL)
-                    .error(Glide.with(context).load(errorUrl))
+                    .load(ERROR_URL)
+                    .error(Glide.with(context).load(backupUrlWhenLoadUrlError))
                     .into(imageView)
             }),
-            ButtonModel("显示fallback地址(url==null)", View.OnClickListener {
+            ButtonModel("url==null则显示fallback图片", View.OnClickListener {
                 // 2.3 load() 方法 url 应该不是 null ，但是如果有可能为 null 的情况，
                 //     你可以通过设置 fallback() 方法来显示 url 为 null 的图片，
                 val optionsFallback: RequestOptions = RequestOptions()
@@ -85,7 +100,9 @@ class GlideMainActivity : BaseActivity() {
                     .error(R.drawable.ic_error)
                     .fallback(R.drawable.ic_fallback)
                     .override(com.bumptech.glide.request.target.Target.SIZE_ORIGINAL)
-                Glide.with(context).load(IMAGE_SMALL).apply(optionsFallback).into(imageView)
+
+                val nullUrl: String? = null
+                Glide.with(context).load(nullUrl).apply(optionsFallback).into(imageView)
 
                 // 上面 2.3 例子 图片显示流程为：
                 //      1. 正在加载 url 的时候，显示图片 ic_default。
