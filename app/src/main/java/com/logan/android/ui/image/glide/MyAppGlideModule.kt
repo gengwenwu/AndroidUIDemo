f5b5613f3ff798f05064484241da8e5a2b7c0583
Logan <490211581@qq.com>
2020-06-03 16:11:47 +0800

    添加glide:compiler

---
+++ app/src/main/java/com/logan/android/ui/image/glide/MyAppGlideModule.kt
@@ -0,0 +1,13 @@
+package com.logan.android.ui.image.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * desc: Glide 4.0 之前版本的用法 <br/>
+ * time: 2020/6/2 5:58 PM <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+@GlideModule
+class MyAppGlideModule : AppGlideModule()
