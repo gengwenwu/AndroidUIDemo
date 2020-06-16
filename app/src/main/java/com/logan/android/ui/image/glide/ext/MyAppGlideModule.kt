6453cd75dcb2eac1cf35373b9b3a66625311573f
Logan <490211581@qq.com>
2020-06-16 16:07:35 +0800

    Glide配置代码注释完善

--- app/src/main/java/com/logan/android/ui/image/glide/ext/MyAppGlideModule.kt
+++ app/src/main/java/com/logan/android/ui/image/glide/ext/MyAppGlideModule.kt
@@ -6,6 +6,7 @@ import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.engine.cache.ExternalPreferredCacheDiskCacheFactory
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import com.logan.android.ui.tool.log
@@ -16,8 +17,8 @@ import java.io.InputStream
  *
  * 注意：
  * 一个项目（包含主项目与依赖库）中只能存在一个继承AppGlideModule的自定义模块，如果有多个，则会报：
- *  com.android.dex.DexException: Multiple dex files define Lcom/bumptech/glide/GeneratedAppGlideModuleImpl异常。
- *
+ *  com.android.dex.DexException:
+ *      Multiple dex files define Lcom/bumptech/glide/GeneratedAppGlideModuleImpl异常。
  *
  * time: 2020/6/2 5:58 PM <br/>
  * author: Logan <br/>
@@ -28,34 +29,38 @@ import java.io.InputStream
 class MyAppGlideModule : AppGlideModule() {
 
     /**
-     * 主要修改Glide默认配置，如：缓存配置
+     * 修改Glide默认配置，如：内存缓存、磁盘缓存配置等。
+     * 没有特殊要求，不建议重新配置，因为 Glide 会自动根据手机配置进行分配。
+     *
+     *  可以看 GlideBuilder -> build() 源码，该函数中所有的配置都可以在 applyOptions() 中配置。
+     *  并且下面的 applyOptions() 优先与 GlideBuilder.build() 执行。
      **/
     override fun applyOptions(context: Context, builder: GlideBuilder) {
         log("=== MyAppGlideModule -> applyOptions() ")
 
-        // 1，内存缓存配置（不建议配置，Glide 会自动根据手机配置进行分配）
-        //    1.1 设置内存缓存大小
-        // builder.setMemoryCache(LruResourceCache(350 * 1024L))
-        //    2.1 设置Bitmap池大小
-        // builder.setBitmapPool(LruBitmapPool(350 * 1024L))
+        // 1，内存缓存配置
+        //    1.1 配置内存缓存策略，默认配置是：LruResourceCache
+        //          builder.setMemoryCache(LruResourceCache(350 * 1024L))
+        //    2.1 设置Bitmap缓存池，默认配置是：LruBitmapPool 或 BitmapPoolAdapter
+        //          builder.setBitmapPool(LruBitmapPool(350 * 1024L))
 
 
-        // 2，磁盘缓存配置（默认缓存大小250M，默认保存在内部存储中）
-        //    2.1 设置磁盘缓存保存在外部存储，且指定缓存大小
-        // TODO Logan ExternalPreferredCacheDiskCacheFactory 还有哪些？
-        // builder.setDiskCache(ExternalPreferredCacheDiskCacheFactory(context, 300 * 1024L))
+        // 2，磁盘缓存配置
+        //    2.1 配置硬盘缓存策略，默认配置是：InternalCacheDiskCacheFactory， 250 MB
+        //          builder.setDiskCache(ExternalPreferredCacheDiskCacheFactory(context))
 
         //   2.2 设置磁盘缓存保存在自己指定的目录下，且指定缓存大小
-//        builder.setDiskCache(
-//            DiskLruCacheFactory(
-//                DiskLruCacheFactory.CacheDirectoryGetter {
-//                    val diskCacheFolder = File("自定义保存路径")
-//                    diskCacheFolder
-//                },
-//                diskCacheSize
-//            )
-//        )
+//          builder.setDiskCache(
+//              DiskLruCacheFactory(
+//                  DiskLruCacheFactory.CacheDirectoryGetter {
+//                      val diskCacheFolder = File("自定义保存路径")
+//                      diskCacheFolder
+//                  },
+//                  diskCacheSize
+//              )
+//          )
 
+        // 等等
     }
 
     /**
